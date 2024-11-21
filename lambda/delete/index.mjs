1028a077ba4461dcf8e82f4b55241d1f4cc31b21
Lev Giffune <levgiffune@gmail.com>
2024-11-20 21:42:00 -0500

    /delete fully debugged for managers and admin

--- lambda/delete/index.mjs
+++ lambda/delete/index.mjs
@@ -10,7 +10,7 @@ export const handler = async (event) => {
     
     let isAdmin = (cred) =>{
         return new Promise((resolve, reject) => {
-            pool.query('SELECT credential FROM restaurants WHERE username=admin', [cred], (error, row) => {
+            pool.query('SELECT credential FROM restaurants WHERE username=\'admin\'', [cred], (error, row) => {
                 if(error){reject("database error")}
                 if(row.length == 1 && row[0].credential == cred){resolve(true)}
                 resolve(false)
@@ -22,16 +22,17 @@ export const handler = async (event) => {
         return new Promise((resolve, reject) => {
             pool.query('SELECT username FROM restaurants WHERE credential=?', [cred], (error, row) => {
                 if(error){reject("database error")}
-                if(row.length != 1 || row[0].credential != credential){resolve("insufficient permissions")}
-                resolve(row[0].username)
+                if(row.length != 1){reject("no such restaurant")}
+                resolve(row[0])
             })
         })
     };
 
     let delRestaurant = (username) =>{
         return new Promise((resolve, reject) => {
-            pool.query('DELETE FROM restaurants WHERE USERNAME=?', [username], (error) => {
-                if(error){reject("Failed to Delete")}
+            pool.query('DELETE FROM restaurants WHERE username=?', [username], (error, num) => {
+                if(error){reject("failed to delete")}
+                if(num == 0){reject("no such restaurant")}
                 resolve(username)
             })
         })
@@ -42,16 +43,16 @@ export const handler = async (event) => {
     try{
         const owner = await getRestaurant(event.credential)
         const admin = await isAdmin(event.credential)
-        if(admin || owner == event.username){
-            const del = delRestaurant(event.username)
+        if(admin || owner.username == event.username){
+            const del = await delRestaurant(event.username)
             response = {
                 statusCode: 200,
-                success: del
+                success: "deleted " + del
             }
         }else{
             response = {
                 statusCode: 400,
-                credential: "invalid restauraunt"
+                credential: "insufficient permissions"
             }
         }
     }catch(err){
