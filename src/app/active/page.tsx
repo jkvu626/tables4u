33a756df8696b3e7a372be6357827a9603a85af1
Lev Giffune <levgiffune@gmail.com>
2024-11-20 23:42:08 -0500

    fixed build issues with direct manipulation of `document.cookie`

--- src/app/active/page.tsx
+++ src/app/active/page.tsx
@@ -24,7 +24,8 @@ const Table: React.FC = () => (
 
 const Active: React.FC = () => {
   const router = useRouter();
-
+  const [credential, setCred] = React.useState('');
+  React.useEffect(() => {setCred(document.cookie)}, [])
   const handleAvailability = () => {
     router.push('/availability');
   };
@@ -32,7 +33,7 @@ const Active: React.FC = () => {
   return (
       <div className='content-create'>
       <div className='createbox'>
-          <h2>{document.cookie}</h2>
+          <h2>{credential}</h2>
           <div className='createbox'>
               <Table/>
               <Table/>
