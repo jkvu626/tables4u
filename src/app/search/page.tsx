a9deae61de4068c7e3fa78993beedad41a075c0d
Daniel Mastrobuono <dlmastrobuono@wpi.edu>
2024-11-25 14:13:44 -0500

    Mostly completed the list active restaurants. Needs UI work.

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,33 @@
+'use client'
 import React from 'react';
 import Restaurant from '../../components/Restaurant';
+import axios from 'axios';
 
-const Search: React.FC = () => (
+const instance = axios.create({
+  baseURL:'https://92ouj9flzf.execute-api.us-east-2.amazonaws.com/tables4u/'
+})
+
+
+const Search: React.FC = () => {
+  const [restaurants, setRestaurants] = React.useState([]);
+
+
+  React.useEffect(() => {
+    instance.get("/restaurants").then(function (response){
+      const status = response.data.statusCode;
+      if (status == 200) {
+        setRestaurants(Object.values(response.data.restaurants))
+      }
+      else {
+        setRestaurants([])
+      }
+    })
+  
+  }, [])
+
+
+  return(
+
   <div className='content'>
     <div className="filter">
       <label>Filter</label>
@@ -16,11 +42,15 @@ const Search: React.FC = () => (
       </label>
     </div>
     <div className="searchbox">
-      <Restaurant />
-      <Restaurant />
-      <Restaurant />
+      {restaurants?.map(({name, date, address}) => (
+        <Restaurant 
+        key={name}
+        name={name} 
+        date={date}
+        address={address}/>
+      ))}
     </div>
   </div>
-);
+);}
 
 export default Search;
