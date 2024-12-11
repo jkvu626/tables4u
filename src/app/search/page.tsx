96b087ea52406e9cd6c65211fbaefd46a15f5b96
Daniel Mastrobuono <dlmastrobuono@wpi.edu>
2024-12-11 14:33:50 -0500

    Yo chat

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Restaurant from '../../components/Restaurant';
 import axios from 'axios';
+import InputField from '@/components/InputField'
 
 const instance = axios.create({
   baseURL:'https://92ouj9flzf.execute-api.us-east-2.amazonaws.com/tables4u/'
@@ -10,7 +11,10 @@ const instance = axios.create({
 
 const Search: React.FC = () => {
   const [restaurants, setRestaurants] = React.useState([]);
-
+  const [filterDate, setDate] = React.useState(new Date());
+  const [closedates, setClosedDates] = React.useState<any[]>([]);
+  const [filterName, setFilterName] = React.useState('')
+  const [filterTime, setFilterTime] = React.useState(0)
 
   React.useEffect(() => {
     instance.get("/restaurants").then(function (response){
@@ -25,35 +29,78 @@ const Search: React.FC = () => {
   
   }, [])
 
+  const handleFilter = () => {
+    alert("whaddup beast")
+    const name = document.getElementById("name") as HTMLInputElement
+    const time = document.getElementById("time") as HTMLInputElement
+    const date = document.getElementById("date") as HTMLInputElement
+    const newDate = new Date(date.value)
+    if(name && time && date) {
+      setFilterName(name.value)
+      setFilterTime(parseInt(time.value))
+      setDate(newDate)
+    }
+  }
+
+  React.useEffect(() => {
+    instance.get("/get_closed", {})
+    .then(function (response){
+        console.log(response.data.dates)
+        const status = response.data.statusCode;
+        if (status == 200) {
+            setClosedDates(response.data.dates)
+        } else {
+            console.log(response.data.err)
+            console.log("TABLE ERROR")
+            setClosedDates([])
+        }
+    })
+}, [])
+
+const Filter = (
+  restaurants: Array<{ name: string; open: number; close: number; address: string; username: string }>,
+  name: string,
+  currentTime: number,
+  date: Date | undefined
+) => {
+  return restaurants.filter((restaurant) => {
+    const matchesName = name ? restaurant.name.toLowerCase().includes(name.toLowerCase()) : true;
+    const isOpen = currentTime ? currentTime >= restaurant.open && currentTime <= restaurant.close : true;
+    const checkDate = date
+    ? !closedates.some((closedate) => 
+      closedate.username === restaurant.username && 
+      closedate.month === date.getMonth() + 1 &&
+      closedate.day === date.getDate() + 1 && 
+      closedate.year === date.getFullYear())
+    : true;
+    return matchesName && isOpen && checkDate
+  });
+};
 
   return(
-
-  <div className='content'>
-    <div className="filter">
-      <label>Filter</label>
-      <label>
-        Name: <input type="text" />
-      </label>
-      <label>
-        Date: <input type="text" />
-      </label>
-      <label>
-        Time: <input type="text" />
-      </label>
+    <div className='content'>
+      <div className="filter">
+        <label>Filter</label>
+        <InputField label="Name:" placeholder='' id='name'/>
+        <label>
+          Date: <input type="date" id='date'/>
+        </label>
+        <InputField label="Time:" placeholder='' id='time'/>
+        <button onClick={handleFilter}>Filter</button>
+      </div>
+      <div className="searchbox">
+        {Filter(restaurants, filterName, filterTime, filterDate)?.map(({name, open, close, address, username}) => (
+          <Restaurant
+          key={name}
+          name={name}
+          open={open}
+          close={close}
+          address={address}
+          username={username}
+          />
+        ))}
+      </div>
     </div>
-    <div className="searchbox">
-      {restaurants?.map(({name, open, close, address, username}) => (
-        <Restaurant 
-        key={name}
-        name={name} 
-        open={open}
-        close={close}
-        address={address}
-        username={username}
-        />
-      ))}
-    </div>
-  </div>
-);}
+  );}
 
 export default Search;
